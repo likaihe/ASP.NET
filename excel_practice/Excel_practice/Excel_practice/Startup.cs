cadd8250fc83a882ade1e85c5f60893657665fad
likaihe <1047561425@qq.com>
2016-11-30 18:16:55 +1300

    get started

---
+++ excel_practice/Excel_practice/Excel_practice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Excel_practice.Startup))]
+namespace Excel_practice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
